9546ec97969f7205ca69b0ec86d438645c453e5c
Hari Priya <kataripriya1@gmail.com>
2022-03-24 10:44:39 +0530

    First commit...

---
+++ src/com/cg/demo/Test1.java
@@ -0,0 +1,13 @@
+package com.cg.demo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String str = "Happy";
+		if (str.equals("Happy"))
+			System.out.println("Go out...");
+		else
+			System.out.println("Stay Home");
+	}
+
+}
